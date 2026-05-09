43219ec2e8f2e69cbf5c26a7e2cf1d973df728cb
nao <>
2026-05-09 18:03:41 +0900

    バリデーションエラーメッセージの位置を各項目の上部に変更

--- resources/views/auth/register/register.blade.php
+++ resources/views/auth/register/register.blade.php
@@ -1,26 +1,18 @@
 <x-guest-layout>
   <form action="{{ route('registerPost') }}" method="POST">
-    <!-- バリデーションエラーメッセージ表示 -->
-    @if ($errors->any())
-      <div class="alert alert-danger" style="width:25%; margin:0 auto;">
-        <ul class="m-0">
-          @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-          @endforeach
-        </ul>
-      </div>
-    @endif
     <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center;">
       <div class="w-25 vh-75 border p-3">
         <div class="register_form">
           <div class="d-flex mt-3" style="justify-content:space-between">
             <div class="" style="width:140px">
+              @error('over_name')<span class="text-danger" style="font-size:11px">{{ $message }}</span>@enderror
               <label class="d-block m-0" style="font-size:13px">姓</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 over_name" name="over_name">
               </div>
             </div>
             <div class="" style="width:140px">
+              @error('under_name')<span class="text-danger" style="font-size:11px">{{ $message }}</span>@enderror
               <label class=" d-block m-0" style="font-size:13px">名</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 under_name" name="under_name">
@@ -29,12 +21,14 @@
           </div>
           <div class="d-flex mt-3" style="justify-content:space-between">
             <div class="" style="width:140px">
+              @error('over_name_kana')<span class="text-danger" style="font-size:11px">{{ $message }}</span>@enderror
               <label class="d-block m-0" style="font-size:13px">セイ</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 over_name_kana" name="over_name_kana">
               </div>
             </div>
             <div class="" style="width:140px">
+              @error('under_name_kana')<span class="text-danger" style="font-size:11px">{{ $message }}</span>@enderror
               <label class="d-block m-0" style="font-size:13px">メイ</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 under_name_kana" name="under_name_kana">
@@ -42,6 +36,7 @@
             </div>
           </div>
           <div class="mt-3">
+            @error('mail_address')<span class="text-danger" style="font-size:11px">{{ $message }}</span>@enderror
             <label class="m-0 d-block" style="font-size:13px">メールアドレス</label>
             <div class="border-bottom border-primary">
               <input type="mail" class="w-100 border-0 mail_address" name="mail_address">
@@ -49,6 +44,7 @@
           </div>
         </div>
         <div class="mt-3">
+          @error('sex')<span class="text-danger" style="font-size:11px">{{ $message }}</span>@enderror
           <input type="radio" name="sex" class="sex" value="1">
           <label style="font-size:13px">男性</label>
           <input type="radio" name="sex" class="sex" value="2">
@@ -57,6 +53,9 @@
           <label style="font-size:13px">その他</label>
         </div>
         <div class="mt-3">
+          @error('old_year')<span class="text-danger" style="font-size:11px">{{ $message }}</span>@enderror
+          @error('old_month')<span class="text-danger" style="font-size:11px">{{ $message }}</span>@enderror
+          @error('old_day')<span class="text-danger" style="font-size:11px">{{ $message }}</span>@enderror
           <label class="d-block m-0 aa" style="font-size:13px">生年月日</label>
           <select class="old_year" name="old_year">
             <option value="none">-----</option>
@@ -141,6 +140,7 @@
           <label style="font-size:13px">日</label>
         </div>
         <div class="mt-3">
+          @error('role')<span class="text-danger" style="font-size:11px">{{ $message }}</span>@enderror
           <label class="d-block m-0" style="font-size:13px">役職</label>
           <input type="radio" name="role" class="admin_role role" value="1">
           <label style="font-size:13px">教師(国語)</label>
@@ -161,6 +161,7 @@
           @endforeach
         </div>
         <div class="mt-3">
+          @error('password')<span class="text-danger" style="font-size:11px">{{ $message }}</span>@enderror
           <label class="d-block m-0" style="font-size:13px">パスワード</label>
           <div class="border-bottom border-primary">
             <input type="password" class="border-0 w-100 password" name="password">
