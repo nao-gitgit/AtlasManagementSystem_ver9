c8afb6543f8b492d832fa6ad02023008e3391ec4
nao <>
2026-05-20 20:29:14 +0900

    一覧画面で選択科目の表示

--- resources/views/authenticated/users/search.blade.php
+++ resources/views/authenticated/users/search.blade.php
@@ -44,6 +44,10 @@
       <div>
         @if($user->role == 4)
         <span>選択科目 :</span>
+        <!-- 選択科目を表示 -->
+        @foreach($user->subjects as $subject)
+        <span>{{ $subject->subject }}</span>
+        @endforeach
         @endif
       </div>
     </div>
