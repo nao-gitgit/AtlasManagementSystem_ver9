ce0f468caa201e3857e7ebd4949ad2121cf39d71
nao <>
2026-06-02 23:57:36 +0900

    レイアウト調整【ユーザー検索】 ・ユーザーの詳細画面の選択科目の登録の部分に矢印実装

--- resources/views/authenticated/users/profile.blade.php
+++ resources/views/authenticated/users/profile.blade.php
@@ -14,7 +14,7 @@
       </div>
       <div class="">
         @can('admin')
-        <span class="subject_edit_btn">選択科目の編集</span>
+        <span class="subject_edit_btn">選択科目の登録 ∧</span>
         <div class="subject_inner">
           <form action="{{ route('user.edit') }}" method="post">
             @foreach($subject_lists as $subject_list)
